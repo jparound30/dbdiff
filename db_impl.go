8744b628f7f7e7fff0bfa17e239f0319f05e2f5a
jparound30 <japanese.around30@gmail.com>
2018-09-23 09:51:53 +0900

    Fix comments.

--- db_impl.go
+++ db_impl.go
@@ -70,155 +70,92 @@ func (holder *DBManager) Close() error {
 	return err
 }
 
-// PingContext verifies a connection to the database is still alive,
-// establishing a connection if necessary.
+// Wrapper for sql.DB#PingContext()
 func (holder *DBManager) PingContext(ctx context.Context) error {
 	return holder.db.PingContext(ctx)
 }
 
-// Ping verifies a connection to the database is still alive,
-// establishing a connection if necessary.
+// Wrapper for sql.DB#Ping()
 func (holder *DBManager) Ping() error {
 	return holder.db.PingContext(context.Background())
 }
 
-// SetMaxIdleConns sets the maximum number of connections in the idle
-// connection pool.
-//
-// If MaxOpenConns is greater than 0 but less than the new MaxIdleConns,
-// then the new MaxIdleConns will be reduced to match the MaxOpenConns limit.
-//
-// If n <= 0, no idle connections are retained.
-//
-// The default max idle connections is currently 2. This may change in
-// a future release.
+// Wrapper for sql.DB#SetMaxIdleConns()
 func (holder *DBManager) SetMaxIdleConns(n int) {
 	holder.db.SetMaxIdleConns(n)
 }
 
-// SetMaxOpenConns sets the maximum number of open connections to the database.
-//
-// If MaxIdleConns is greater than 0 and the new MaxOpenConns is less than
-// MaxIdleConns, then MaxIdleConns will be reduced to match the new
-// MaxOpenConns limit.
-//
-// If n <= 0, then there is no limit on the number of open connections.
-// The default is 0 (unlimited).
+// Wrapper for sql.DB#SetMaxOpenConns()
 func (holder *DBManager) SetMaxOpenConns(n int) {
 	holder.db.SetMaxOpenConns(n)
 }
 
-// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-//
-// Expired connections may be closed lazily before reuse.
-//
-// If d <= 0, connections are reused forever.
+// Wrapper for sql.DB#SetConnMaxLifetime()
 func (holder *DBManager) SetConnMaxLifetime(d time.Duration) {
 	holder.db.SetConnMaxLifetime(d)
 }
 
-// Stats returns database statistics.
+// Wrapper for sql.DB#Stats()
 func (holder *DBManager) Stats() sql.DBStats {
 	return holder.db.Stats()
 }
 
-// PrepareContext creates a prepared statement for later queries or executions.
-// Multiple queries or executions may be run concurrently from the
-// returned statement.
-// The caller must call the statement's Close method
-// when the statement is no longer needed.
-//
-// The provided context is used for the preparation of the statement, not for the
-// execution of the statement.
+// Wrapper for sql.DB#PrepareContext()
 func (holder *DBManager) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
 	return holder.db.PrepareContext(ctx, query)
 }
 
-// Prepare creates a prepared statement for later queries or executions.
-// Multiple queries or executions may be run concurrently from the
-// returned statement.
-// The caller must call the statement's Close method
-// when the statement is no longer needed.
+// Wrapper for sql.DB#Prepare()
 func (holder *DBManager) Prepare(query string) (*sql.Stmt, error) {
 	return holder.db.PrepareContext(context.Background(), query)
 }
 
-// ExecContext executes a query without returning any rows.
-// The args are for any placeholder parameters in the query.
+// Wrapper for sql.DB#ExecContext()
 func (holder *DBManager) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
 	return holder.db.ExecContext(ctx, query, args)
 }
 
-// Exec executes a query without returning any rows.
-// The args are for any placeholder parameters in the query.
+// Wrapper for sql.DB#Exec()
 func (holder *DBManager) Exec(query string, args ...interface{}) (sql.Result, error) {
 	return holder.db.ExecContext(context.Background(), query, args...)
 }
 
-// QueryContext executes a query that returns rows, typically a SELECT.
-// The args are for any placeholder parameters in the query.
+// Wrapper for sql.DB#QueryContext()
 func (holder *DBManager) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
 	return holder.db.QueryContext(ctx, query, args)
 }
 
-// Query executes a query that returns rows, typically a SELECT.
-// The args are for any placeholder parameters in the query.
+// Wrapper for sql.DB#Query()
 func (holder *DBManager) Query(query string, args ...interface{}) (*sql.Rows, error) {
 	return holder.db.QueryContext(context.Background(), query, args...)
 }
 
-// QueryRowContext executes a query that is expected to return at most one row.
-// QueryRowContext always returns a non-nil value. Errors are deferred until
-// Row's Scan method is called.
-// If the query selects no rows, the *Row's Scan will return ErrNoRows.
-// Otherwise, the *Row's Scan scans the first selected row and discards
-// the rest.
+// Wrapper for sql.DB#QueryRowContext()
 func (holder *DBManager) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
 	return holder.db.QueryRowContext(ctx, query, args)
 }
 
-// QueryRow executes a query that is expected to return at most one row.
-// QueryRow always returns a non-nil value. Errors are deferred until
-// Row's Scan method is called.
-// If the query selects no rows, the *Row's Scan will return ErrNoRows.
-// Otherwise, the *Row's Scan scans the first selected row and discards
-// the rest.
+// Wrapper for sql.DB#QueryRow()
 func (holder *DBManager) QueryRow(query string, args ...interface{}) *sql.Row {
 	return holder.db.QueryRowContext(context.Background(), query, args...)
 }
 
-// BeginTx starts a transaction.
-//
-// The provided context is used until the transaction is committed or rolled back.
-// If the context is canceled, the sql package will roll back
-// the transaction. Tx.Commit will return an error if the context provided to
-// BeginTx is canceled.
-//
-// The provided TxOptions is optional and may be nil if defaults should be used.
-// If a non-default isolation level is used that the driver doesn't support,
-// an error will be returned.
+// Wrapper for sql.DB#BeginTx()
 func (holder *DBManager) BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) {
 	return holder.db.BeginTx(ctx, opts)
 }
 
-// Begin starts a transaction. The default isolation level is dependent on
-// the driver.
+// Wrapper for sql.DB#Begin()
 func (holder *DBManager) Begin() (*sql.Tx, error) {
 	return holder.db.BeginTx(context.Background(), nil)
 }
 
-// Driver returns the database's underlying driver.
+// Wrapper for sql.DB#Driver()
 func (holder *DBManager) Driver() driver.Driver {
 	return holder.db.Driver()
 }
 
-// Conn returns a single connection by either opening a new connection
-// or returning an existing connection from the connection pool. Conn will
-// block until either a connection is returned or ctx is canceled.
-// Queries run on the same Conn will be run in the same database session.
-//
-// Every Conn must be returned to the database pool after use by
-// calling Conn.Close.
+// Wrapper for sql.DB#Conn()
 func (holder *DBManager) Conn(ctx context.Context) (*sql.Conn, error) {
 	return holder.db.Conn(ctx)
 }
